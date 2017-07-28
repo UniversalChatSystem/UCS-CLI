b68b63f2791b7ee9d9f16dd211c969fec6f76b8a
HittmanA <hittmana@comcast.net>
2017-07-27 21:04:53 -0500

    Fixed the CLI to work with the new server code and added correct ip and port.

--- cli.js
+++ cli.js
@@ -1,11 +1,12 @@
+#!/usr/bin/env node
 const WebSocket = require('ws');
 
 var timestamp = require('time-stamp');
-    
+
 const chalk = require('chalk');
- 
-var port = 8080; //The same port that the server is listening on 
-var host = 'localhost'/*'138.197.114.14'*/;
+
+var port = 3001; //The same port that the server is listening on
+var host = '138.197.114.14'/*'138.197.114.14'*/;
 var socket = new WebSocket('ws://'+host+":"+port, {
   origin: 'http://'+host
 });
@@ -14,17 +15,17 @@ socket.sendJson = function(data){
 }
 var username = "none";
 var hasUsername = false;
-socket.on('open', function() { //Don't send until we're connected 
+socket.on('open', function() { //Don't send until we're connected
 
     log("Connected to host (" + host + ":" + port + ")");
     log("Please enter a username:");
     rl.prompt();
-    
+
 });
 
 socket.on('message', function(message){
     var data = JSON.parse(message);
-    if(data.command == "say")
+    if(data.command == "broadcast")
     {
         if(data.data.sender != username) {
             log(data.data.sender + ": " + data.data.message);
@@ -36,18 +37,18 @@ socket.on('message', function(message){
 const readline = require('readline');
 
 var rl = readline.createInterface({
-    
+
   input: process.stdin,
   output: process.stdout,
   prompt: '> '
-  
+
 });
 
 rl.on('line', (line) => {
     if(hasUsername == true)
     {
         socket.sendJson({command: "sendMessage", data: {sender: username, message: line}});
-        
+
         rl.prompt();
     } else {
         socket.sendJson({command: "username", data: {sender: line}});
@@ -56,12 +57,12 @@ rl.on('line', (line) => {
         rl.setPrompt("["+line+"]: ");
         rl.prompt();
     }
-    
+
 }).on('close', () => {
-    
+
   console.log('Have a great day!');
   process.exit(0);
-  
+
 });
 
 var log = function(message)
